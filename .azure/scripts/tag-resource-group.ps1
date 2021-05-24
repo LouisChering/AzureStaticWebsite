14fe43905a7338536747dcb87315812a6c2a3d4f
Louis Chering <louis@orbitalwitness.com>
2021-05-24 20:54:42 +0100

    pr fix

--- .azure/scripts/tag-resource-group.ps1
+++ .azure/scripts/tag-resource-group.ps1
@@ -1,13 +1,16 @@
 param(
     $ResourceGroupName,
     $Environment,
-    $Expires,
-    $OwnerName,
-    $OwnerEmail,
-    $PR,
-    $GitSha
+    $PR
 )
 
+$gitsha=$(git rev-parse --short HEAD)
+$gitshortsha=$(git rev-parse --short $gitsha)
+$owner=$(git log -1 --pretty=format:'%an')
+$ownerEmail=$(git log -1 --pretty=format:'%ae')
+$branch=$(git branch --show-current)
+$expires=$(date -d "+1 week" +%Y%m%d)
+
 $rg = Get-AzResourceGroup -Name $ResourceGroupName
-$tag = @{Environment=$Environment; Expires=$Expires; Branch=$Branch; OwnerEmail=$OwnerEmail; PRId=$PR; GitSha=$GitSha; PR='true'}
+$tag = @{Environment=$Environment; Expires=$expires; Branch=$Branch; Owner=$owner; OwnerEmail=$ownerEmail; PRId=$PR; GitSha=$gitshortsha; PR='true'}
 New-AZTag -ResourceId $rg.ResourceId -Tag $tag -Verbose
