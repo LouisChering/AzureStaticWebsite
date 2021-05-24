1d633e1ec6cd615bd2f0e05bd85cf9416d888df6
Louis Chering <louis@orbitalwitness.com>
2021-05-24 20:44:07 +0100

    pipeline param fixes

--- .azure/scripts/tag-resource-group.ps1
+++ .azure/scripts/tag-resource-group.ps1
@@ -5,10 +5,9 @@ param(
     $OwnerName,
     $OwnerEmail,
     $PR,
-    $GitSha,
-    $Branch
+    $GitSha
 )
 
 $rg = Get-AzResourceGroup -Name $ResourceGroupName
-$tag = @{Environment=$Environment; Expires=$Expires; Branch=$Branch; OwnerName=$OwnerName; OwnerEmail=$OwnerEmail; PRId=$PR; GitSha=$GitSha; PR='true'}
+$tag = @{Environment=$Environment; Expires=$Expires; Branch=$Branch; OwnerEmail=$OwnerEmail; PRId=$PR; GitSha=$GitSha; PR='true'}
 New-AZTag -ResourceId $rg.ResourceId -Tag $tag -Verbose
