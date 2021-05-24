8ea192ab7510d4780bf0f63fdb94bd5607900fed
Louis Chering <louis@orbitalwitness.com>
2021-05-24 16:56:43 +0100

    typo

--- .azure/scripts/tag-resource-group.ps1
+++ .azure/scripts/tag-resource-group.ps1
@@ -9,5 +9,5 @@ param(
 )
 
 $rg = Get-AzResourceGroup -Name $ResourceGroupName
-$tag = @{Environment='$Environment'; Expires='$Expires'; OwnerName='$OwnerName'; OwnerEmail='$OwnerEmail'; PRId='$PR'; GitSha='$GitSha'; PR='true'}
+$tag = @{Environment=$Environment; Expires=$Expires; OwnerName=$OwnerName; OwnerEmail=$OwnerEmail; PRId=$PR; GitSha=$GitSha; PR='true'}
 New-AZTag -ResourceId $rg.ResourceId -Tag $tag -Verbose
